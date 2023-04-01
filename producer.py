07aa1844d90873e1ecac8b9adcc0d75bc71b28a4
Chufan Chen <allenplato28@gmail.com>
2023-04-01 13:05:16 +0000

    init

---
+++ producer.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+from random import choice
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Producer
+import cv2
+import os
+
+if __name__ == '__main__':
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    args = parser.parse_args()
+
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+
+    # Create Producer instance
+    producer = Producer(config)
+
+    # Optional per-message delivery callback (triggered by poll() or flush())
+    # when a message has been successfully delivered or permanently
+    # failed delivery (after retries).
+    def delivery_callback(err, msg):
+        if err:
+            print('ERROR: Message failed delivery: {}'.format(err))
+        else:
+            print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+    # Produce data by selecting random values from these lists.
+    topic = "my_image_topic"
+
+    count = 0
+    for filename in os.listdir('path/to/images'):
+        # Read image using OpenCV
+        image = cv2.imread(os.path.join('path/to/images', filename))
+        # Convert image to bytes
+        _, img_encoded = cv2.imencode('.jpg', image)
+        message = img_encoded.tobytes()
+        
+        producer.produce(topic, value=message, callback=delivery_callback)
+        count += 1
+
+    # Block until the messages are sent.
+    producer.poll(10000)
+    producer.flush()
