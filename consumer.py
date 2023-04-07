a13f6d285bfbfb883cd0e71e5a98e53feca99f33
Chufan Chen <allenplato28@gmail.com>
2023-04-07 17:56:27 +0800

    numpy to base64

--- consumer.py
+++ consumer.py
@@ -5,6 +5,30 @@ import numpy as np
 from argparse import ArgumentParser, FileType
 from configparser import ConfigParser
 from confluent_kafka import Consumer, OFFSET_BEGINNING
+from middleware import send_service
+from concurrent.futures import ThreadPoolExecutor
+
+url = "http://10.14.42.236:32492/imageClassification"
+
+# Define the function to process a Kafka message
+def process_message(msg):
+    frame_byted = msg.value()
+    numpy_array = np.frombuffer(frame_byted)
+    result = numpy_array
+    # result = send_service(url, numpy_array).json()
+    print("Processed message with result:", result)
+
+# Define the function to consume messages from Kafka
+def consume_messages():
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                print("Waiting...")
+            elif msg.error():
+                print("ERROR: %s".format(msg.error()))
+            else:
+                executor.submit(process_message, msg)
 
 if __name__ == '__main__':
     # Parse the command line.
@@ -36,23 +60,7 @@ if __name__ == '__main__':
 
     # Poll for new messages from Kafka and print them.
     try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is None:
-                # Initial message consumption may take up to
-                # `session.timeout.ms` for the consumer group to
-                # rebalance and start consuming
-                print("Waiting...")
-            elif msg.error():
-                print("ERROR: %s".format(msg.error()))
-            else:
-                # Extract the (optional) key and value, and print.
-                frame_byted = msg.value()
-                numpy_array = np.frombuffer(frame_byted)
-                print("Consumed event from topic {topic}: key = {key:12} value = {value}".format(
-                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=numpy_array))
-                
-            
+        consume_messages()
     except KeyboardInterrupt:
         pass
     finally:
--- middleware.py
+++ middleware.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://10.14.42.236:32492/imageClassification"
+url = "https://62a03266-fc4f-4a06-aab9-89e4e0ad3145.mock.pstmn.io/imageClassification"
 data = {"data": "5.0,3.4,1.5,0.2"}
 
 def send_service(url, data):
@@ -15,12 +15,30 @@ def send_service(url, data):
     response = requests.post(url, headers=headers, data=data_json)
     
     print(response.status_code) # Should print 200 if successful
-    print(response.json()) # Should print the response data from the server
+    try:
+        json_data = json.loads(response.text)
+        print(json_data)
+    except json.decoder.JSONDecodeError:
+        # handle the case where the response body could not be decoded as JSON
+        print("Failed to decode response body as JSON")
+
+    
 
     return response
 
-response = send_service(url, data)
+def numpy_to_base64_image(arr):
+    # Reshape the 1D array to a 3D array of shape (32, 32, 3)
+    arr = np.uint8(arr * 255)
+    img_arr = arr.reshape((3, 32, 32)).transpose([1, 2, 0]) 
+    # Convert the NumPy array to a PIL Image object
+    img = Image.fromarray(img_arr)
+    # Create a BytesIO object to hold the image data
+    img_bytes = io.BytesIO()
+    # Save the image to the BytesIO object in PNG format
+    img.save(img_bytes, format='PNG')
+    # Encode the image data in base64
+    img_base64 = base64.b64encode(img_bytes.getvalue()).decode('utf-8')
+    # Return the base64-encoded image string
+    return img_base64
 
-
-print(response.status_code) # Should print 200 if successful
-print(response.json()) # Should print the response data from the server
+# send_service("http://10.14.42.236:32032/imageClassification", )
